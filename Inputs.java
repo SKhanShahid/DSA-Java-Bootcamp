f83e37f378eec72b2065883c42ea514854383637
SKhanShahid <skhanshahid84@gmail.com>
2025-10-27 21:03:26 +0530

    first java program

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Inputs{
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("enter your rollno:");
+            int rollno = input.nextInt();
+            System.out.println("your rollno is: " + rollno);
+        }
+    }
+}
