bd53c6cda26fe354adf1462a4be8aa4d8397c54a
SKhanShahid <skhanshahid84@gmail.com>
2025-11-01 11:17:43 +0530

    .gitignore modified CaseCheck.java modified

--- conditionals & Loops/CaseCheck.java
+++ conditionals & Loops/CaseCheck.java
@@ -4,9 +4,16 @@ public class CaseCheck{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
         char ch = sc.next().trim().charAt(0);
-        if(ch >='a' && ch <='z'){
+        // if(ch >='a' && ch <='z'){
+        //     System.out.println(ch + " is a lowercase letter.");
+        // } else if(ch >='A' && ch <='Z'){
+        //     System.out.println(ch + " is an uppercase letter.");
+        // } else {
+        //     System.out.println(ch + " is not an alphabetic letter.");
+        // }
+        if(Character.isLowerCase(ch)){
             System.out.println(ch + " is a lowercase letter.");
-        } else if(ch >='A' && ch <='Z'){
+        } else if(Character.isUpperCase(ch)){
             System.out.println(ch + " is an uppercase letter.");
         } else {
             System.out.println(ch + " is not an alphabetic letter.");
