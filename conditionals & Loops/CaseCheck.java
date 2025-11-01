1cbfc19e25f49101380dbcb45c779ff64302d611
SKhanShahid <skhanshahid84@gmail.com>
2025-11-01 11:17:04 +0530

    CaseChecker added

---
+++ conditionals & Loops/CaseCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CaseCheck{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().trim().charAt(0);
+        if(ch >='a' && ch <='z'){
+            System.out.println(ch + " is a lowercase letter.");
+        } else if(ch >='A' && ch <='Z'){
+            System.out.println(ch + " is an uppercase letter.");
+        } else {
+            System.out.println(ch + " is not an alphabetic letter.");
+        }
+        sc.close();
+        
+    }
+}
