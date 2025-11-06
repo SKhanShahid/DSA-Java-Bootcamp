d8e14afc329430c790493b4a1c8a616b9fcb729a
SKhanShahid <skhanshahid84@gmail.com>
2025-11-06 22:51:46 +0530

    Methods eg for Sum

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sum{
+    public static void main(String[] args) {
+        sumTwoNumbers();
+        sumTwoNumbers();
+    }
+    static void sumTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+        scanner.close();
+    }
+}
