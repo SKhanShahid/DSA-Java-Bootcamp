07fc005eb3f255c1e72e1a4b6cad7a0379d67a2f
SKhanShahid <skhanshahid84@gmail.com>
2025-11-08 15:33:53 +0530

    ntg much

--- Methods/Sum.java
+++ Methods/Sum.java
@@ -1,18 +1,24 @@
+
 import java.util.Scanner;
 
 public class Sum {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        sumTwoNumbers(scanner);
-        scanner.close();
+    // Method to calculate the sum of two integers
+    public static int calculateSum(int a, int b) {
+        return a + b;
     }
 
-    static void sumTwoNumbers(Scanner scanner) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
+
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
+
+        int sum = calculateSum(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        scanner.close();
     }
 }
