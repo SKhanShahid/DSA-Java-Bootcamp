c523a5b106c5b30e0581950078140b142e53a3c9
SKhanShahid <skhanshahid84@gmail.com>
2025-11-19 19:41:14 +0530

    swapping method to use XOR bitwise operation instead of a temporary variable.

--- Arrays/Swap.java
+++ Arrays/Swap.java
@@ -1,14 +1,14 @@
-package Arrays;
+
 import java.util.Arrays;
 public class Swap{
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        swap(arr, 1, 3);
+        swap(arr, 0, 4);
         System.out.println(Arrays.toString(arr));
     }
-    static void swap(int[] arr, int index1, int index2){
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+    static void swap(int[] arr, int i, int j){
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
     }
 }
