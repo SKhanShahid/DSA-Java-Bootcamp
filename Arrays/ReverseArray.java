e0b300046a3acdf980540766de64d4bf0cdb1f96
SKhanShahid <skhanshahid84@gmail.com>
2025-11-20 00:26:21 +0530

    reversing an array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+public class ReverseArray{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        reverse(arr);
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+    static void reverse(int[] arr){
+        int start = 0, end = arr.length - 1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+}
