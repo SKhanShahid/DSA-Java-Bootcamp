4aeb7529852609e104d0b9518486d2696022d739
SKhanShahid <skhanshahid84@gmail.com>
2026-04-30 12:12:36 +0530

    ceiling of number using binarysearch

--- BinarySearch/ceiling.java
+++ BinarySearch/ceiling.java
@@ -1,7 +1,7 @@
 public class ceiling{
     public static void main(String[] args) {
         int[] arr = {2,3,5,9,14,16,18,20};
-        int target = 4;
+        int target = 15;
         int ans = CeilinglinearSearch(arr, target);
         System.out.println(ans);
         }
