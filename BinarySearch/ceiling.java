3d6e4eb5413e67a856161cac27d72d4013d3f387
SKhanShahid <skhanshahid84@gmail.com>
2026-04-30 10:52:06 +0530

    added ceiling of number using linearsearch

---
+++ BinarySearch/ceiling.java
@@ -0,0 +1,20 @@
+public class ceiling{
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18,20};
+        int target = 4;
+        int ans = CeilinglinearSearch(arr, target);
+        System.out.println(ans);
+        }
+         static int CeilinglinearSearch(int[] arr, int target){
+            if(arr.length == 0){
+                return -1;
+            }
+            for(int index = 0; index < arr.length; index++){
+                if(arr[index] >= target){
+                    return arr[index];
+                }
+            }
+            return -1;
+        
+    }
+}
